79f2e06bdfb175c2488d864dfc81236bd4a8f3c4
marquez138 <somose138@gmail.com>
2025-10-13 12:47:35 -1000

    11.2

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,13 +1,13 @@
 // app/(auth)/sign-in/[[...sign-in]]/page.tsx
 'use client'
 
-// import { SignIn } from '@clerk/nextjs'
+import { SignIn } from '@clerk/nextjs'
 
-// export default function SignInPage() {
-//   return (
-//     <div className='mx-auto max-w-md p-6'>
-//       {/* Redirect to our role-aware landing page */}
-//       <SignIn redirectUrl='/post-sign-in' />
-//     </div>
-//   )
-// }
+export default function SignInPage() {
+  return (
+    <div className='mx-auto max-w-md p-6'>
+      {/* Redirect to our role-aware landing page */}
+      <SignIn redirectUrl='/post-sign-in' />
+    </div>
+  )
+}
